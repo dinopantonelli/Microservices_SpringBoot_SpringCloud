37f5a7a64895f8ea321c7dc966fe91c78c10348a
Dino Antonelli <dinoantonelli@hotmail.com>
2022-07-12 12:07:24 -0300

    eureka

--- bike-service/src/main/java/dino/microservice/BikesServiceApplication.java
+++ bike-service/src/main/java/dino/microservice/BikesServiceApplication.java
@@ -2,8 +2,10 @@ package dino.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class BikesServiceApplication {
 
 	public static void main(String[] args) {
