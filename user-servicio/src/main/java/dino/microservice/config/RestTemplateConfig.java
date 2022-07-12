df35548f55aaae837dcb825593d4225d044dec2a
Dino Antonelli <dinoantonelli@hotmail.com>
2022-07-12 15:46:33 -0300

    eureka LoadBalanced

--- user-servicio/src/main/java/dino/microservice/config/RestTemplateConfig.java
+++ user-servicio/src/main/java/dino/microservice/config/RestTemplateConfig.java
@@ -3,11 +3,13 @@ package dino.microservice.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 
 @Configuration
 public class RestTemplateConfig {
 	
 	 @Bean // para poder inyectarlo
+	 @LoadBalanced
 		public RestTemplate restTemplate(){
 
 			return new RestTemplate();
--- user-servicio/src/main/java/dino/microservice/feignclients/CarFeignClient.java
+++ user-servicio/src/main/java/dino/microservice/feignclients/CarFeignClient.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import dino.microservice.model.Cars;
 
-@FeignClient(name = "car-service", url="http://localhost:8089")
+@FeignClient(name = "car-service")
 @RequestMapping("/cars")
 
 public interface CarFeignClient {
